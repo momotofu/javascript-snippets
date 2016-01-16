95b5a9996c2b81afda28e1a5ce7d9c962a62ff25
Christopher Reece <mrkita.reece@gmail.com>
2016-01-16 16:17:05 -0700

    optimized output with control flow

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,11 +1,14 @@
 var fizzBuzz = function(count) {
   var total = count; 
   while (total > 0) {
-    var Fizz = total % 3 == 0 ? 'Fizz' : '';
-    var Buzz = total % 5 == 0 ? 'Buzz' : '';
-    var fester = !Fizz && !Buzz ? 'spsss..' : '';
-    var FizzBuzz = Fizz + Buzz; 
-    console.log(Fizz + Buzz + fester);
+    var output = '';
+    if (total % 3 == 0)
+      output += 'Fizz';
+    if (total % 5 == 0)
+      output += 'Buzz';
+    if (!output) 
+      output = 'spsss..';
+    console.log(output);
     total -= 1;
   }
 }
