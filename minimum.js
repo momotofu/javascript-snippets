94f6d9e11a76b9cb19e131e6b9bb5a6c09fefdbc
Christopher Reece <mrkita.reece@gmail.com>
2016-01-19 22:05:02 -0700

    added minimum.js

---
+++ minimum.js
@@ -0,0 +1,10 @@
+// Define a function that takes two arguments 
+// and returns the minimum 
+
+function minimum(number, comparison) {
+  return number > comparison ? comparison : number; 
+}
+
+console.log(minimum(200, 5));
+console.log(minimum(2324, 3245));
+console.log(minimum(-1, -6));
