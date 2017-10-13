eb9c161e21bf36b4499a0933031ba312aab2b928
Christopher Reece <mrkita.reece@gmail.com>
2017-10-14 01:20:03 +0900

    added sudoku.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
-import * as exercises from './coat_hangers/1-exercises'
+import * as sudoku from '../archive/sudoku.js'
 
+debugger;
+console.log('app div', document.getElementById('app'), App);
 ReactDOM.render(<App />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,22 @@
 const {resolve} = require('path')
 
 module.exports = {
-  entry: './main.js',
+  entry: resolve(__dirname, 'src/main.js'),
   output: {
     filename: 'bundle.js',
     path: resolve(__dirname, 'bin')
   },
   context: resolve(__dirname, 'src'),
-  devServer: {
-    inline: true,
-    port: 9000
-  },
   module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel',
-        exclude: [
-          resolve(__dirname, 'node_modules')
-        ],
-        include: [
-          resolve(__dirname, 'src')
-        ],
-        query: {
-          presets: ['es2015', 'react']
-        }
-      },
-      {test: /\.css$/, loader: 'style!css'}
-    ]
-  },
-  node: {
-    fs: "empty"
+    loaders: [{
+      test: /\.js$/,
+      loader: "babel",
+      include: [
+        resolve(__dirname, 'src')
+      ],
+      query: {
+        presets: [ "es2015", "react", "react-hmre" ]
+      }
+    }]
   }
 }
