33d0804ebc9b3d96ef05a68232a1459e747f2020
Christopher Reece <mrkita.reece@gmail.com>
2017-10-16 11:02:23 +0900

    removed junky code

--- archive/chapter001-3/chessboard.js
+++ archive/chapter001-3/chessboard.js
@@ -1,27 +1,3 @@
-module.exports.generateBoard = function(width, height) {
-  var board = '';
-  var rowCount = height; 
-  var colCount = width;
-  var hash = true;
-
-  var addPiece = function() {
-    board += !!hash ? '#' : ' ';
-    hash = !!hash ? false : true; 
-  }
-  do {
-    for (var i = colCount; i > 0; i--) {
-      addPiece();
-      if (i === 1) {
-        board += '\n';
-      }
-    }
-    rowCount -= 1; 
-  } while (rowCount > 1);
-  console.log(board);
-}
-
-// generateBoard(311, 150);
-
 module.exports.generateSquareBoard = function(size) {
   board = '';
   for (var x = size; x > 0; x--) {
