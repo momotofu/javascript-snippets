97d4788f02717ef351982b09d3ab1d35d47814eb
Christopher Reece <mrkita.reece@gmail.com>
2017-10-16 12:34:37 +0900

    refactored minimum.js for node

--- archive/chapter001-3/minimum.js
+++ archive/chapter001-3/minimum.js
@@ -1,13 +1,9 @@
 // Define a function that takes two arguments 
 // and returns the minimum 
 
-function minimum(number, comparison) {
+module.exports.minimum = function (number, comparison) {
   return number > comparison ? comparison : number; 
 }
-
-console.log(minimum(200, 5));
-console.log(minimum(2324, 3245));
-console.log(minimum(-1, -6));
 
 /** remember the trouble you had with 
 this one? All answer do not need to 
