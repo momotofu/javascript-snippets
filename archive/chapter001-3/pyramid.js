6f2140ddbeb5a47776d50fa9061d382652facba0
Christopher Reece <mrkita.reece@gmail.com>
2017-10-16 12:44:50 +0900

    removed construct pyramid function

--- archive/chapter001-3/pyramid.js
+++ archive/chapter001-3/pyramid.js
@@ -1,35 +1,42 @@
-var buildTriangle = function(base) {
-    var leftBricks = '';
-    var rightBricks = ''; 
-    var counter = base / 2; 
+module.exports = (() => { 
+    var buildTriangle = function(base) {
+        var leftBricks = '';
+        var rightBricks = ''; 
+        var counter = base / 2; 
 
-    // fill up leftBricks with empty strings
-  for(var i = counter; i > 0; i--) {
-      leftBricks += ' '; 
+        // fill up leftBricks with empty strings
+        for(var i = counter; i > 0; i--) {
+          leftBricks += ' '; 
+        }
+
+        //place the tip
+        console.log(leftBricks + '#');
+
+        // build triangle 
+        do {
+            rightBricks += '#';
+            leftBricks = leftBricks.substr(0, counter) + '#' + leftBricks.substr(counter+1);
+            counter -= 1; 
+            console.log(leftBricks + rightBricks);
+        } while (counter > 0); 
     }
     
-    //place the tip
-    console.log(leftBricks + '#');
-  
-  // build triangle 
-    do {
-        rightBricks += '#';
-        leftBricks = leftBricks.substr(0, counter) + '#' + leftBricks.substr(counter+1);
-        counter -= 1; 
-        console.log(leftBricks + rightBricks);
-    } while (counter > 0); 
-}
+    function constructPyramids() {
+        var numberOfPyramids = 10;
+        var base = 3;
 
-var numberOfPyramids = 10;
-var base = 3;
-
-do {
-    buildTriangle(base);
-    base *= 2;
-    numberOfPyramids -= 1; 
-} while (numberOfPyramids > 0);
+        do {
+            buildTriangle(base);
+            base *= 2;
+            numberOfPyramids -= 1; 
+        } while (numberOfPyramids > 0);
+    }
+        
+    return {
+        buildTriangle: buildTriangle,
+        constructPyramids: constructPyramids
+    }
+})();
 
 /** build oblisiks to compliment the pyramid.
-Can I make do while purposefull? **/ 
-
-
+Can I make do while purposefull? **/
