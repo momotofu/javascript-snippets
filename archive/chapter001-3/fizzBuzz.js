8bbdb17a3c8fe3fce91174cfeb4b4ec4ec51124e
Christopher Reece <mrkita.reece@gmail.com>
2017-10-16 11:38:00 +0900

    exposed fizzbuzz to node, reformatted code

--- archive/chapter001-3/fizzBuzz.js
+++ archive/chapter001-3/fizzBuzz.js
@@ -1,19 +1,21 @@
-var fizzBuzz = function(count) {
-  var total = count; 
-  while (total > 0) {
-    var output = '';
-    if (total % 3 == 0)
-      output += 'Fizz';
-    if (total % 5 == 0)
-      output += 'Buzz';
-    if (!output) 
-      output = 'spsss..';
-    console.log(output);
-    total -= 1;
-  }
-}
-
-fizzBuzz(100);
+module.exports = (() => {
+    return {
+        fizzBuzz: function(count) {
+            var total = count; 
+            while (total > 0) {
+                var output = '';
+                if (total % 3 == 0)
+                  output += 'Fizz';
+                if (total % 5 == 0)
+                  output += 'Buzz';
+                if (!output) 
+                  output = 'spsss..';
+                console.log(output);
+                total -= 1;
+            }
+        }
+    }
+})();
 
 /** write out all the different
 ways this can be accomplished.
