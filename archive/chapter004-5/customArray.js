b1d4bfa30d4a9ee0c41033edcfe60b4e91492b60
Christopher Reece <mrkita.reece@gmail.com>
2017-11-04 19:26:15 +0900

    refined customArray.js

--- archive/chapter004-5/customArray.js
+++ archive/chapter004-5/customArray.js
@@ -4,18 +4,20 @@ exports.array = {
   length: 0,
 
   push: function(n) {
-    items[length] = n; 
-    length += 1;
+    this.items[this.length] = n; 
+    this.length += 1;
   },
 
   pop: function(p) {
-    delete items[length]; 
-    length -= 1; 
+    current = this.items[this.length];
+    delete this.items[this.length]; 
+    this.length -= 1;
+    return current;
   },
 
   indexOf: function(n) {
-    for (var index in items) {
-      if (items[index] === n) {
+    for (var index in this.items) {
+      if (this.items[index] === n) {
         return index;
       }
     }
