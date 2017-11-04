2d114fadc40bcce7e3121c49d1c74be8aedaecda
Christopher Reece <mrkita.reece@gmail.com>
2017-11-04 19:10:12 +0900

    fixed spacing

--- archive/chapter004-5/higher-order-functions.js
+++ archive/chapter004-5/higher-order-functions.js
@@ -1,7 +1,7 @@
 /**
 Terms:
 
-Notes: Fnctions that operate on other functions, 
+Notes: Functions that operate on other functions, 
 either by taking them as arguments or by returning them, 
 are called higher-order functions.
 
