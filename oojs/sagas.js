27682622405422ff8bf1ebb19a3a9d8384189ca2
Christopher Reece <mrkita.reece@gmail.com>
2017-12-18 18:12:36 +0900

    demonstrating closures

--- oojs/sagas.js
+++ oojs/sagas.js
@@ -4,13 +4,17 @@
  * calls of the same function.
  */
 // Global scope
+var sagas = []
 var hero = "Gal"
 var newSaga = function() {
   // function scope 1
   var foil = "Cat"
-  var saga = function () {
+  var sagas.push(function () {
     // function scope 2
     var deed = "Pet"
     log(hero+deed+foil)
-  }
+  })
 }
+newSaga()
+sagas[0]()
+sagas[0]()
