99beac1759bd263dc8548ffc85e6d873963faf44
Christopher Reece <mrkita.reece@gmail.com>
2017-12-18 21:10:49 +0900

    adding animations on click

--- design-patterns/cat-clicker/js/index.js
+++ design-patterns/cat-clicker/js/index.js
@@ -1,5 +1,6 @@
 document.getElementById('cat-image-container')
-  .addEventListener('click', () => {
+  .addEventListener('click', function() {
+    this.classList.add('buzz-out')
     el = document.getElementById('number-of-clicks')
     elCount = parseInt(el.innerText)
     el.innerText =  elCount ? elCount + 1 : 1
