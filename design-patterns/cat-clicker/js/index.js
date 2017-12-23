263afcf83247b96608cb9ca4c58ee7f3859afdfc
Christopher Reece <mrkita.reece@gmail.com>
2017-12-23 21:08:35 +0900

    changed main to index.js

--- design-patterns/cat-clicker/js/index.js
+++ design-patterns/cat-clicker/js/index.js
@@ -1,6 +1,10 @@
 document.getElementById('cat-image-container')
   .addEventListener('click', function() {
-    this.classList.add('buzz-out')
+    if (this.classList.contains('buzz-out')) {
+      this.classList.remove('buzz-out').add('buzz-out')
+    } else {
+      this.classList.add('buzz-out')
+    }
     el = document.getElementById('number-of-clicks')
     elCount = parseInt(el.innerText)
     el.innerText =  elCount ? elCount + 1 : 1
