21055d9f94d98204baa344269e63a8edaef89ea4
Christopher Reece <mrkita.reece@gmail.com>
2017-12-27 17:48:29 +0900

    set up js for counters

--- design-patterns/cat-clicker/js/index.js
+++ design-patterns/cat-clicker/js/index.js
@@ -1,10 +1,10 @@
 const catContainerList = document.getElementsByClassName('hero-container')
 
-for (var key in catContainerList) {
+for (let key in catContainerList) {
   catContainerList[key].addEventListener('mousedown', function() {
     const list = this.classList
     list.add('buzz-out')
-    el = document.getElementById('number-of-clicks')
+    el = document.getElementById(`number-of-clicks-${key}`)
     elCount = parseInt(el.innerText)
     el.innerText =  elCount ? elCount + 1 : 1
   })
@@ -12,6 +12,5 @@ for (var key in catContainerList) {
   catContainerList[key].addEventListener('mouseup', function() {
     const list = this.classList
     list.remove('buzz-out')
-    console.log('mouseup')
   })
 }
