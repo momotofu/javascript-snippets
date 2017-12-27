51924414cad1d79b5e2ba6d90538a8f0287f520a
Christopher Reece <mrkita.reece@gmail.com>
2017-12-27 17:55:31 +0900

    fixed undefined error

--- design-patterns/cat-clicker/js/index.js
+++ design-patterns/cat-clicker/js/index.js
@@ -1,16 +1,18 @@
 const catContainerList = document.getElementsByClassName('hero-container')
 
 for (let key in catContainerList) {
-  catContainerList[key].addEventListener('mousedown', function() {
-    const list = this.classList
-    list.add('buzz-out')
-    el = document.getElementById(`number-of-clicks-${key}`)
-    elCount = parseInt(el.innerText)
-    el.innerText =  elCount ? elCount + 1 : 1
-  })
+  if (!isNaN(parseInt(key))) {
+    catContainerList[key].addEventListener('mousedown', function() {
+      const list = this.classList
+      list.add('buzz-out')
+      el = document.getElementById(`number-of-clicks-${key}`)
+      elCount = parseInt(el.innerText)
+      el.innerText =  elCount ? elCount + 1 : 1
+    })
 
-  catContainerList[key].addEventListener('mouseup', function() {
-    const list = this.classList
-    list.remove('buzz-out')
-  })
+    catContainerList[key].addEventListener('mouseup', function() {
+      const list = this.classList
+      list.remove('buzz-out')
+    })
+  }
 }
