fd430d2cf3e7b3fea2225687ae8a43e23bcfb4f3
Christopher Reece <mrkita.reece@gmail.com>
2017-12-27 17:37:05 +0900

    added second cat image

--- design-patterns/cat-clicker/js/index.js
+++ design-patterns/cat-clicker/js/index.js
@@ -1,15 +1,17 @@
-const catContainer = document.getElementById('cat-image-container')
+const catContainerList = document.getElementsByClassName('hero-container')
 
-catContainer.addEventListener('mousedown', function() {
-  const list = this.classList
-  list.add('buzz-out')
-  el = document.getElementById('number-of-clicks')
-  elCount = parseInt(el.innerText)
-  el.innerText =  elCount ? elCount + 1 : 1
-})
+for (var key in catContainerList) {
+  catContainerList[key].addEventListener('mousedown', function() {
+    const list = this.classList
+    list.add('buzz-out')
+    el = document.getElementById('number-of-clicks')
+    elCount = parseInt(el.innerText)
+    el.innerText =  elCount ? elCount + 1 : 1
+  })
 
-catContainer.addEventListener('mouseup', function() {
-  const list = this.classList
-  list.remove('buzz-out')
-  console.log('mouseup')
-})
+  catContainerList[key].addEventListener('mouseup', function() {
+    const list = this.classList
+    list.remove('buzz-out')
+    console.log('mouseup')
+  })
+}
