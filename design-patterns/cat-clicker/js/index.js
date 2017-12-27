fc99914ad2bdde7e28863baf89edbd2b3a817821
Christopher Reece <mrkita.reece@gmail.com>
2017-12-27 17:00:09 +0900

    fixed cat animation by adding mouseup and mousedown event listeners

--- design-patterns/cat-clicker/js/index.js
+++ design-patterns/cat-clicker/js/index.js
@@ -1,13 +1,15 @@
-document.getElementById('cat-image-container')
-  .addEventListener('click', function() {
-    if (this.classList.contains('buzz-out')) {
-      this.classList.remove('buzz-out').add('buzz-out')
-    } else {
-      this.classList.add('buzz-out')
-    }
-    el = document.getElementById('number-of-clicks')
-    elCount = parseInt(el.innerText)
-    el.innerText =  elCount ? elCount + 1 : 1
-  })
+const catContainer = document.getElementById('cat-image-container')
 
+catContainer.addEventListener('mousedown', function() {
+  const list = this.classList
+  list.add('buzz-out')
+  el = document.getElementById('number-of-clicks')
+  elCount = parseInt(el.innerText)
+  el.innerText =  elCount ? elCount + 1 : 1
+})
 
+catContainer.addEventListener('mouseup', function() {
+  const list = this.classList
+  list.remove('buzz-out')
+  console.log('mouseup')
+})
