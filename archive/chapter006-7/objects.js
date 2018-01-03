d165f22b86e442d6b57facfeef2d2a744e2a1323
Christopher Reece <mrkita.reece@gmail.com>
2018-01-03 15:25:21 +0900

    exposed talkToRabbits

--- archive/chapter006-7/objects.js
+++ archive/chapter006-7/objects.js
@@ -1,5 +1,5 @@
 function speak(line) {
-  console.log('The ' + this.type + ' rabbit says, \"' + line + '\"'); 
+  console.log('The ' + this.type + ' rabbit says, \"' + line + '\"');
 }
 
 var redRabbit = {
@@ -10,26 +10,30 @@ var tibbarDer = {
   type: 'der'
 }
 
-//polymorphism 
-var rabbits = [redRabbit, tibbarDer]; 
+const peter = {
+  type: 'rabbit'
+}
 
-rabbits.forEach(function(n) {
-  speak.call(n, 'I\'m a ' + n.type + ' kind of rabbit.'); 
-})
+//polymorphism
+var rabbits = [redRabbit, tibbarDer];
+
+module.exports.talkToRabbits = () => {
+  rabbits.forEach(function(n) {
+    speak.call(n, 'I\'m a ' + n.type + ' kind of rabbit.');
+  })
+}
 
 
-speak.apply(redRabbit, ['I\'m as good as backwards dear sir.', 'There\'s a snake in my boot']); 
+speak.apply(redRabbit, ['I\'m as good as backwards dear sir.', 'There\'s a snake in my boot']);
 
 var mechanicalRabbit = {
   type: 'mechanical',
-  speak: function(line) { 
+  speak: function(line) {
     console.log('The ' + this.type + ' rabbit says, \'' + line);
   },
-  fuelType: 'oil' 
+  fuelType: 'oil'
 };
 
 mechanicalRabbit.speak('hello');
-var mechaRabbit = Object.create(mechanicalRabbit); 
+var mechaRabbit = Object.create(mechanicalRabbit);
 console.log(mechaRabbit.speak('Sqreeeech'));
-
-
