32d264a67726c13a0ea29228719d6d6064059064
Christopher Reece <mrkita.reece@gmail.com>
2018-01-17 16:13:33 +0900

    fixed variable naming

--- design-patterns/cat-clicker/js/index.js
+++ design-patterns/cat-clicker/js/index.js
@@ -5,9 +5,9 @@ for (let key in catContainerList) {
     catContainerList[key].addEventListener('mousedown', function() {
       const list = this.classList
       list.add('buzz-out')
-      el = document.getElementById(`number-of-clicks-${key}`)
-      elCount = parseInt(el.innerText)
-      el.innerText =  elCount ? elCount + 1 : 1
+      counterTextEl = document.getElementById(`number-of-clicks-${key}`)
+      currentCount = parseInt(counterTextEl.innerText)
+      counterTextEl.innerText =  currentCount ? currentCount + 1 : 1
     })
 
     catContainerList[key].addEventListener('mouseup', function() {
